3c86a78c469176b30ce5ff9c6f1daf60b37c83bf
Tim Hutt <tdhutt@gmail.com>
2015-06-07 13:56:02 +0100

    Compilation fixes
    #include <QApplication> instead of <QtGui/QApplication>

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@
  *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
  ***************************************************************************/
 
-#include <QtGui/QApplication>
+#include <QApplication>
 #include "MainWindow.h"
 
 #include "ProgramOptions.h"
