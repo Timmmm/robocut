c670cb7c0d18dff5bdfb3acd896c8e5c786737cf
Daniel Ruoso <daniel@ruoso.com>
2018-03-17 11:39:58 -0400

    Add PRODUCT_ID_SILHOUETTE_CAMEO_3
    The new model has a different product id, I suspect the
    Cameo 2 has a different id as well, but I only have acccess
    to test a Cameo3, which does work correctly with this change

--- Plotter.cpp
+++ Plotter.cpp
@@ -155,6 +155,7 @@ libusb_device_handle *UsbOpen(struct cutter_id *id)
 		     desc.idProduct == PRODUCT_ID_SILHOUETTE_SD_1 ||
 		     desc.idProduct == PRODUCT_ID_SILHOUETTE_SD_2 ||
 		     desc.idProduct == PRODUCT_ID_SILHOUETTE_CAMEO ||
+		     desc.idProduct == PRODUCT_ID_SILHOUETTE_CAMEO_3 ||
 		     desc.idProduct == PRODUCT_ID_SILHOUETTE_PORTRAIT
 		     )
 		    )
@@ -335,7 +336,8 @@ Error Cut(CutParams p)
 	struct cutter_id id = { "?", 0, 0 };
 	libusb_device_handle* handle = UsbInit(&id);
 	if (id.usb_vendor_id == VENDOR_ID_GRAPHTEC &&
-	   id.usb_product_id == PRODUCT_ID_SILHOUETTE_CAMEO)
+	    (id.usb_product_id == PRODUCT_ID_SILHOUETTE_CAMEO ||
+	     id.usb_product_id == PRODUCT_ID_SILHOUETTE_CAMEO_3))
 	  {
 	    // should this also transform the regwidth regheigth or not?
 		p.cuts = Transform_Silhouette_Cameo(p.cuts, &p.mediawidth, &p.mediaheight);
