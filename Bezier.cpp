2e310fbc6e51d9606de855dc91d1fa1e2f4d5d8c
Tim Hutt <tdhutt@gmail.com>
2018-04-15 17:00:35 +0100

    Add temporary bezier -> poly implementation

--- Bezier.cpp
+++ Bezier.cpp
@@ -6,5 +6,15 @@ void addBezierToPolygon(QPolygonF& poly,
                         qreal angleThreshold,
                         qreal distanceThreshold)
 {
-	// TODO
+	// TODO: Calculate N somehow.
+	const int N = 10;
+	for (int i = 1; i < N; ++i)
+	{
+		qreal t = i / (N-1.0);
+		qreal s = 1.0 - t;
+		
+		QPointF x = s*s*s * p1 + 3.0 * s * t * (s * p2 + t * p3) + t*t*t * p4;
+		
+		poly.append(x);
+	}
 }
