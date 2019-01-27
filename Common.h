3b02a33d7e213bfb47b7b20e6f43ddaf8a4a894f
Tim Hutt <tdhutt@gmail.com>
2019-01-27 18:02:31 +0000

    Remove Error class in favour of std::variant (WIP)

--- Common.h
+++ Common.h
@@ -1,40 +1,18 @@
 #pragma once
 
-/// Result Class
-
 #include <string>
 #include <sstream>
 #include <cstdio>
 #include <vector>
 #include <cstdlib>
 #include <limits>
+#include <variant>
 
-using std::vector;
-using std::string;
-
-typedef string::size_type stringpos;
-
-/// Error Handling
-
-class Error
-{
-public:
-	explicit Error(bool S) : s(S) { }
-	explicit Error(const char* C) : s(false), m(C) { }
-	Error(const string& msg) : s(false), m(msg) { }
-	operator bool() { return s; } // TODO: Use bool hack from boost so int i = Error(); doesn't work.
-	string message() { if (m.empty()) return "Unspecified Error"; return m; }
-
-private:
-	bool s;
-	string m;
-};
-
-const Error Success(true);
-const Error Failure(false);
+template<typename R>
+using Result = std::variant<R, std::string>;
 
 // Integer to string.
-inline string ItoS(int I)
+inline std::string ItoS(int I)
 {
 	std::stringstream S;
 	S << I;
@@ -42,7 +20,7 @@ inline string ItoS(int I)
 }
 
 // Unsigned integer to string.
-inline string UItoS(unsigned int I)
+inline std::string UItoS(unsigned int I)
 {
 	std::stringstream S;
 	S << I;
@@ -50,53 +28,53 @@ inline string UItoS(unsigned int I)
 }
 
 // Unsigned long long to string.
-inline string ULLtoS(unsigned long long I)
+inline std::string ULLtoS(unsigned long long I)
 {
 	char out[128];
 	sprintf(out, "%lld", I);
 	return out;
 }
 
-// String to integer, returns Fail on fail.
-inline int StoI(const string& S, int Fail = 0)
+// String to integer.
+inline Result<int> StoI(const std::string& S)
 {
 	char* EP;
 	const char* P = S.c_str();
 	auto R = strtol(P, &EP, 0);
 	if (EP == P)
-		return Fail;
+		return "Couldn't convert string to integer: " + S;
 	if (R < std::numeric_limits<int>::lowest() ||
 	    R > std::numeric_limits<int>::max())
-		return Fail;
+		return "Integer out of range: " + S;
 	return static_cast<int>(R);
 }
 
 // String to unsigned integer, returns Fail on fail.
-inline unsigned int StoUI(const string& S, unsigned int Fail = 0)
+inline Result<unsigned int> StoUI(const std::string& S)
 {
 	char* EP;
 	const char* P = S.c_str();
 	auto R = strtoul(P, &EP, 0);
 	if (EP == P)
-		return Fail;
+		return "Couldn't convert string to integer: " + S;
 	if (R > std::numeric_limits<unsigned int>::max())
-		return Fail;
+		return "Integer out of range: " + S;
 	return static_cast<unsigned int>(R);
 }
 
 // String to unsigned long long, returns Fail on fail.
-inline unsigned long long StoULL(const string& S, unsigned long long Fail = 0)
+inline Result<unsigned long long> StoULL(const std::string& S)
 {
 	char* EP;
 	const char* P = S.c_str();
 	auto R = strtoull(P, &EP, 0);
 	if (EP == P)
-		return Fail;
+		return "Couldn't convert string to integer: " + S;
 	return R;
 }
 
 // Get an environmental variable.
-inline string GetEnv(const string& Var)
+inline std::string GetEnv(const std::string& Var)
 {
 	char* V = getenv(Var.c_str());
 	if (!V)
