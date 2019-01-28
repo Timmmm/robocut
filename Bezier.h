61137d5378e247798880a962c1296f9a683097df
Tim Hutt <tdhutt@gmail.com>
2019-01-28 20:37:47 +0000

    Comment addBezierToPolygon a little

--- Bezier.h
+++ Bezier.h
@@ -3,6 +3,8 @@
 #include <QPoint>
 #include <QPolygonF>
 
+// Given a bezier curve defined by the points p1-p4, convert it
+// to a polyline with the given fineness, and apprent it to poly.
 void addBezierToPolygon(QPolygonF& poly,
                         const QPointF& p1, const QPointF& p2,
                         const QPointF& p3, const QPointF& p4,
