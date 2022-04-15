acf5c533e31a104b0c4d25db469ad377e92c1c48
Tim Hutt <tdhutt@gmail.com>
2022-04-15 16:09:55 +0100

    Update linux config.h

--- libusb/linux_config/config.h
+++ libusb/linux_config/config.h
@@ -1,16 +1,30 @@
-/* Generated on Ubuntu 16.04, VERSION and PACKAGE_VERSION removed. */
+/* Generated on Ubuntu 20.04. */
 
-/* Default visibility */
-#define DEFAULT_VISIBILITY __attribute__((visibility("default")))
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Start with debug message logging enabled */
+/* Define to the attribute for default visibility. */
+#define DEFAULT_VISIBILITY __attribute__ ((visibility ("default")))
+
+/* Define to 1 to start with debug message logging enabled. */
 /* #undef ENABLE_DEBUG_LOGGING */
 
-/* Message logging */
+/* Define to 1 to enable message logging. */
 #define ENABLE_LOGGING 1
 
 /* Define to 1 if you have the <asm/types.h> header file. */
 /* #undef HAVE_ASM_TYPES_H */
+
+/* Define to 1 if you have the `clock_gettime' function. */
+#define HAVE_CLOCK_GETTIME 1
+
+/* Define to 1 if you have the declaration of `EFD_CLOEXEC', and to 0 if you
+   don't. */
+#define HAVE_DECL_EFD_CLOEXEC 1
+
+/* Define to 1 if you have the declaration of `EFD_NONBLOCK', and to 0 if you
+   don't. */
+#define HAVE_DECL_EFD_NONBLOCK 1
 
 /* Define to 1 if you have the declaration of `TFD_CLOEXEC', and to 0 if you
    don't. */
@@ -23,26 +37,36 @@
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
+/* Define to 1 if the system has eventfd functionality. */
+#define HAVE_EVENTFD 1
+
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <IOKit/usb/IOUSBHostFamilyDefinitions.h> header
+   file. */
+/* #undef HAVE_IOKIT_USB_IOUSBHOSTFAMILYDEFINITIONS_H */
 
 /* Define to 1 if you have the `udev' library (-ludev). */
 #define HAVE_LIBUDEV 1
 
-/* Define to 1 if you have the <libudev.h> header file. */
-#define HAVE_LIBUDEV_H 1
-
-/* Define to 1 if you have the <linux/netlink.h> header file. */
-/* #undef HAVE_LINUX_NETLINK_H */
-
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
+
+/* Define to 1 if the system has the type `nfds_t'. */
+#define HAVE_NFDS_T 1
 
 /* Define to 1 if you have the `pipe2' function. */
 #define HAVE_PIPE2 1
 
-/* Define to 1 if you have the <poll.h> header file. */
-#define HAVE_POLL_H 1
+/* Define to 1 if you have the `pthread_condattr_setclock' function. */
+#define HAVE_PTHREAD_CONDATTR_SETCLOCK 1
+
+/* Define to 1 if you have the `pthread_setname_np' function. */
+#define HAVE_PTHREAD_SETNAME_NP 1
+
+/* Define to 1 if you have the `pthread_threadid_np' function. */
+/* #undef HAVE_PTHREAD_THREADID_NP */
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -57,16 +81,10 @@
 #define HAVE_STRING_H 1
 
 /* Define to 1 if the system has the type `struct timespec'. */
-#define HAVE_STRUCT_TIMESPEC 1
+/* #undef HAVE_STRUCT_TIMESPEC */
 
-/* syslog() function available */
-/* #undef HAVE_SYSLOG_FUNC */
-
-/* Define to 1 if you have the <syslog.h> header file. */
-/* #undef HAVE_SYSLOG_H */
-
-/* Define to 1 if you have the <sys/socket.h> header file. */
-/* #undef HAVE_SYS_SOCKET_H */
+/* Define to 1 if you have the `syslog' function. */
+/* #undef HAVE_SYSLOG */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -77,76 +95,62 @@
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
+/* Define to 1 if the system has timerfd functionality. */
+#define HAVE_TIMERFD 1
+
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
 #define LT_OBJDIR ".libs/"
 
-/* Darwin backend */
-/* #undef OS_DARWIN */
-
-/* Haiku backend */
-/* #undef OS_HAIKU */
-
-/* Linux backend */
-#define OS_LINUX 1
-
-/* NetBSD backend */
-/* #undef OS_NETBSD */
-
-/* OpenBSD backend */
-/* #undef OS_OPENBSD */
-
-/* SunOS backend */
-/* #undef OS_SUNOS */
-
-/* Windows backend */
-/* #undef OS_WINDOWS */
-
 /* Name of package */
-#define PACKAGE "libusb"
+#define PACKAGE "libusb-1.0"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "libusb-devel@lists.sourceforge.net"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "libusb"
+#define PACKAGE_NAME "libusb-1.0"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libusb 1.0.22-rc4"
+#define PACKAGE_STRING "libusb-1.0 1.0.26"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libusb"
+#define PACKAGE_TARNAME "libusb-1.0"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL "http://libusb.info"
 
-/* type of second poll() argument */
-#define POLL_NFDS_TYPE nfds_t
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.26"
+
+/* Define to 1 if compiling for a POSIX platform. */
+#define PLATFORM_POSIX 1
+
+/* Define to 1 if compiling for a Windows platform. */
+/* #undef PLATFORM_WINDOWS */
+
+/* Define to the attribute for enabling parameter checks on printf-like
+   functions. */
+#define PRINTF_FORMAT(a, b) __attribute__ ((__format__ (__printf__, a, b)))
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
-/* Use POSIX Threads */
-#define THREADS_POSIX 1
+/* UMockdev hotplug code is not racy */
+/* #undef UMOCKDEV_HOTPLUG */
 
-/* timerfd headers available */
-#define USBI_TIMERFD_AVAILABLE 1
-
-/* Enable output to system log */
+/* Define to 1 to output logging messages to the systemwide log. */
 /* #undef USE_SYSTEM_LOGGING_FACILITY */
 
-/* Use udev for device enumeration/hotplug */
-#define USE_UDEV 1
+/* Version number of package */
+#define VERSION "1.0.26"
 
-/* Oldest Windows version supported */
-/* #undef WINVER */
-
-/* Use GNU extensions */
+/* Enable GNU extensions. */
 #define _GNU_SOURCE 1
 
-/* Oldest Windows version supported */
+/* Define to the oldest supported Windows version. */
 /* #undef _WIN32_WINNT */
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
